0851ad283e651612de96d9410bbea38986fc15c3
Alan Massaro <alan.massar@gmail.com>
2021-12-22 23:26:30 +0100

    Creation of sidebar, header. input tweet wip

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
+        <Sidebar />
+        <Feed />
+      </main>
+
+    </div>
+  )
+}
