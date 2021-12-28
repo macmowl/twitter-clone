a062f5dc43eb3df29a767025285400a9133648c0
Alan Massaro <alan.massar@gmail.com>
2021-12-28 16:01:02 +0100

    final commit

--- firebase.js
+++ firebase.js
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "",
   authDomain: process.env.FIREBASE_KEY,
-  projectId: PROJECT_ID,
+  projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID
